fc6218f8242de6bbdc2c8e9731733d1885e5a0ea
Dmitry Balabka <dmitry.balabka@gmail.com>
2021-06-04 14:56:45 +0300

    Create fillna function for aggregated data

--- shl/prepare.py
+++ shl/prepare.py
@@ -46,6 +46,13 @@ def fill_na(df: pd.DataFrame, limit: int):
     return df
 
 
+def fillna_agg_by_label(df: pd.DataFrame, label: pd.DataFrame, limit: int, epoch_time_col: str):
+    return normalize_epoch_time(label, 'epoch_time')[['epoch_time_id']]\
+        .merge(normalize_epoch_time(df, epoch_time_col), on="epoch_time_id", how='outer') \
+        .fillna(method='ffill', limit=limit, axis=0) \
+        .fillna(method='bfill', limit=limit, axis=0)
+
+
 # Add columns with abs values for selected columns of dataframe
 def calculate_abs_values(df: pd.DataFrame, columns: List[str]):
     df[[f'abs_{col}' for col in columns]] = df[[col for col in columns]].abs()
