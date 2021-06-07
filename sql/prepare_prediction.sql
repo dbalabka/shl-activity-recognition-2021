33389489f8bc2faef7037cee5e691ca8424d5bc0
Denys Shkliarenko <denys.shkliarenko@ecentria.com>
2021-06-07 10:36:09 +0300

    Unpacked label from the list for convenient export to CSV

--- sql/prepare_prediction.sql
+++ sql/prepare_prediction.sql
@@ -1,12 +1,17 @@
-SELECT
+with pred as (
 
-      epoch_time,
+SELECT       
+      epoch_time,      
       --array_agg(tables.score order by tables.score desc limit 1) as max_score,
-      array_agg(tables.value order by tables.score desc limit 1) as predicted_label
-
+      array_agg(tables.value order by tables.score desc limit 1) as predicted_label     
 
 FROM `shl-2021-315220.prediction_dataset_19_distan_20210605062802_2021_06_05T16_11_08_924Z.predictions`,
       unnest(predicted_label) as pred_label
-
 group by epoch_time
+
+)
+
+select epoch_time,
+       predicted_label[ordinal(1)] as predicted_label
+from pred
 order by epoch_time
